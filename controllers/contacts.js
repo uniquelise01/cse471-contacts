64b06928450faaf6ab6c374a70e755351e092334
uniquelise01 <han19008@byui.edu>
2024-04-26 15:57:38 -0600

    getSingle now working

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,8 +10,8 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
-    const contactId = new ObjectId(req.params.id)
-    const result = await mongodb.getDatabase().db().collection('contacts').find(_id, contactId);
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
