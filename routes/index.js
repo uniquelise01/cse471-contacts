9971a989d13d2c244ce890c1005161e1073688ab
uniquelise01 <han19008@byui.edu>
2024-04-26 16:08:50 -0600

    added console.log and server.rest file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => { res.send(console.log('Hello Teacher! Add /contacts in the link for the json list of contacts.')); });
+
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
