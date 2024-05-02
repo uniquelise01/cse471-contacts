244a4a5e8ef23347ab01eabbf2b5510a2513b508
uniquelise01 <han19008@byui.edu>
2024-05-02 15:03:20 -0600

    trying to make swagger tags work

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    //#swagger-tags = ['Contacts']
+    //#swagger-tags=['Contacts']
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    //#swagger-tags = ['Contacts']
+    //#swagger-tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) => {
@@ -21,7 +21,7 @@ const getSingle = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-    //#swagger-tags = ['Contacts']
+    //#swagger-tags=['Contacts']
     const contact = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -38,7 +38,7 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-    //#swagger-tags = ['Contacts']
+    //#swagger-tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
     const contact = {
         firstName: req.body.firstName,
@@ -56,7 +56,7 @@ const updateContact = async (req, res) => {
 };
 
 const deleteContact = async (req, res) => {
-    //#swagger-tags = ['Contacts']
+    //#swagger-tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: contactId});
     if (response.deletedCount > 0) {
