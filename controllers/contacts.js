1c04ebe7b380835628f3fdb38c40d565d4ba9721
uniquelise01 <han19008@byui.edu>
2024-05-09 11:03:07 -0600

    adding error handling

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,20 +1,26 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
+const getAll = (req, res) => {
     //#swagger-tags=['Contacts']
-    const result = await mongodb.getDatabase().db().collection('contacts').find();
-    result.toArray().then((contacts) => {
+    mongodb.getDatabase().db().collection('contacts').find()
+    .toArray((err, lists) => {
+        if (err) {
+            res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
+        res.status(200).json(lists);
     });
 };
 
-const getSingle = async (req, res) => {
+const getSingle = (req, res) => {
     //#swagger-tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
-    result.toArray().then((contacts) => {
+    mongodb.getDatabase().db().collection('contacts').find({_id: contactId})
+    .toArray((err, result) => {
+        if (err) {
+            res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
